f1c905e7e6c2342504f3bb1d7482692a1bdc06d9
marcus8448 <marcus8448_git@outlook.com>
2022-05-09 19:37:38 -0700

    feat: stock spreadsheet generator 0.5.0 queries the absolute minimum amount of data shouldn't fail as much now drops the `yahoo_finance_api` crate config file name for `default.toml` is no longer hardcoded

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,11 @@ use std::process::exit;
 use std::thread::sleep;
 use std::thread::spawn;
 use std::time::Duration;
+use async_compat::CompatExt;
 use chrono::Local;
 use clap::Arg;
 use clap::Command;
 use futures::executor::block_on;
-use yahoo_finance_api::Quote;
-use yahoo_finance_api::YahooConnector;
-use yahoo_finance_api::YahooError;
 use serde::Deserialize;
 use serde::Serialize;
 
@@ -22,7 +20,7 @@ static DEFAULT_CONFIG_NAME: &str = "config.toml";
 
 fn main() {
     let matches = Command::new("Stock Spreadsheet Generator")
-        .version("0.4.0")
+        .version("0.5.0")
         .author("marcus8448")
         .about("Creates a simple spreadsheet based on yahoo finance data")
         .arg(Arg::new("config")
@@ -34,7 +32,6 @@ fn main() {
         )
         .get_matches();
 
-    let provider = YahooConnector::new();
     let filename = matches.value_of("config").unwrap();
 
     let path = std::path::Path::new(filename);
@@ -58,7 +55,7 @@ fn main() {
         Err(error) => panic!("Failed to read config! {:}", error),
     };
 
-    let result: Result<Config, toml::de::Error> = toml::from_str(conf.as_str());
+    let result: Result<Config, toml::de::Error> = toml::from_slice(conf.as_bytes());
     let config = match result {
         Ok(cfg) => cfg,
         Err(error) => panic!("Failed to parse config! {:}", error),
@@ -66,11 +63,7 @@ fn main() {
 
     println!("Query time: {}", Local::now());
 
-    let output_filename: String = if filename == DEFAULT_CONFIG_NAME {
-        "output.csv".to_string()
-    } else {
-        format!("{}{}", filename.replace(".toml", ""), ".csv")
-    };
+    let output_filename = format!("{}{}", filename.replace(".toml", ""), ".csv");
 
     let mut writer = match csv::Writer::from_path(&output_filename) {
         Ok(writer) => writer,
@@ -81,7 +74,7 @@ fn main() {
         }
     };
 
-    for quote in block_on(query_tickers(&config.tickers, &provider)) {
+    for quote in block_on(query_tickers(&config.tickers)) {
         match writer.serialize(quote) {
             Ok(_) => {}
             Err(error) => println!("Warning: Problem writing csv record: - {:?}", error),
@@ -100,7 +93,7 @@ fn main() {
     spawn(|| match open::that(OsString::from(output_filename)) {
         Ok(_) => {}
         Err(_) => {
-            println!("Warning: Failed to open output file!");
+            println!("Error: Failed to open output file!");
             wait_for_input();
         }
     });
@@ -108,74 +101,81 @@ fn main() {
     exit(0);
 }
 
-async fn query_tickers(tickers: &Vec<Ticker>, provider: &YahooConnector) -> Vec<ComparingQuote> {
+async fn query_tickers(tickers: &Vec<Ticker>) -> Vec<FormattedQuote> {
     let mut rows = Vec::new();
     for t in tickers {
-        rows.push(deserialize_yahoo(provider, t).await);
+        rows.push(deserialize_yahoo(t).await);
     }
     rows
 }
 
-async fn deserialize_yahoo(provider: &YahooConnector, t: &Ticker) -> ComparingQuote {
+async fn deserialize_yahoo(t: &Ticker) -> FormattedQuote {
+    struct Values {
+        close: f64,
+        prev_close: f64,
+        currency: String,
+    }
+
     let volume = t.volume.unwrap_or(0);
-    let close = match provider.get_latest_quotes(t.id.as_str(), "1m").await {
-        Ok(quotes) => match quotes.last_quote() {
-            Ok(quote) => quote.close,
+    let quote: Values = match reqwest::get(
+        format!("https://query1.finance.yahoo.com/v8/finance/chart/{symbol}?symbol={symbol}&interval=1m&range=1m&events=div|split", symbol = t.id)
+    ).compat().await {
+        Ok(response) => match response.json().await {
+            Ok(json) => {
+                let response: Response = match serde_json::from_value(json) {
+                    Ok(value) => {
+                        value
+                    }
+                    Err(error) => {
+                        println!("Warning: invalid json! {}", error);
+                        return create_failed_row(t);
+                    }
+                };
+                match response.chart.result.get(0) {
+                    None => {
+                        println!("Warning: missing result!");
+                        return create_failed_row(t);
+                    }
+                    Some(quote) => Values {
+                        close: quote.meta.regular_market_price,
+                        prev_close: quote.meta.previous_close.unwrap_or(quote.meta.chart_previous_close),
+                        currency: quote.meta.currency.clone()
+                    }
+                }
+            }
             Err(error) => {
-                println!("Problem deserializing latest quote: {:?}", error);
+                println!("Warning: malformed json! {}", error);
                 return create_failed_row(t);
             }
-        },
-        Err(error0) => {
-            println!("Warning: Failed to obtain quotes: {:?}", error0);
+        }
+        Err(error) => {
+            println!("Warning: invalid response from yahoo! {}", error);
             return create_failed_row(t);
         }
     };
 
-    let prev_close = match get_yesterday_close(provider, t).await {
-        Ok(quotes) => quotes.get(0).unwrap().close,
-        Err(error) => {
-            println!("Warning: Failed to obtain previous quote: {:?}", error);
-            return ComparingQuote {
-                id: t.id.to_string(),
-                close: Some(format!("{:.2}", close)),
-                change: None,
-                volume: t.volume,
-                total: Some(format!("{:.2}", ((volume as f64) * close)))
-            };
-        }
-    };
-
-    return ComparingQuote {
+    return FormattedQuote {
         id: t.id.to_string(),
-        close: Some(format!("{:.2}", close)),
-        change: Some(format!("{:.2}", close - prev_close)),
-        volume: t.volume,
-        total: Some(format!("{:.2}", ((volume as f64) * close)))
+        close: Some(format!("{:.2}", quote.close)),
+        change: Some(format!("{:.2}", quote.close - quote.prev_close)),
+        amount: t.volume,
+        total: Some(format!("{:.2}", ((volume as f64) * quote.close))),
+        currency: quote.currency
     };
 }
-
-async fn get_yesterday_close(provider: &YahooConnector, t: &Ticker) -> Result<Vec<Quote>, YahooError> {
-    return match provider.get_quote_range(t.id.as_str(), "1d", "2d").await {
-        Ok(data) => {
-            data.quotes()
-        },
-        Err(err) => Err(err)
-    };
-}
-
-fn create_failed_row(t: &Ticker) -> ComparingQuote {
-    ComparingQuote {
+fn create_failed_row(t: &Ticker) -> FormattedQuote {
+    FormattedQuote {
         id: t.id.to_string(),
         close: None,
         change: None,
-        volume: t.volume,
-        total: None
+        amount: t.volume,
+        total: None,
+        currency: "".to_string()
     }
 }
 
 fn wait_for_input() {
-    println!("please press enter to close the window...");
+    println!("Please press enter to close the window...");
     match stdin().read_exact(&mut [0]) {
         Ok(_) => {}
         Err(error) => println!("Warning: Problem reading from stdin: {:?}", error),
@@ -193,14 +193,39 @@ struct Ticker {
     volume: Option<u32>,
 }
 
+#[derive(Deserialize)]
+struct Response {
+    chart: Chart
+}
+
+#[derive(Deserialize)]
+struct Chart {
+    result: Vec<Quote>
+}
+
+#[derive(Deserialize)]
+struct Quote {
+    meta: Meta
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Meta {
+    currency: String,
+    chart_previous_close: f64,
+    previous_close: Option<f64>,
+    regular_market_price: f64
+}
+
 #[derive(Serialize)]
 #[serde(rename_all = "PascalCase")]
-struct ComparingQuote {
+struct FormattedQuote {
     #[serde(rename(serialize = "Ticker"))]
     id: String,
     #[serde(rename(serialize = "Value"))]
     close: Option<String>,
     change: Option<String>,
-    volume: Option<u32>,
+    amount: Option<u32>,
     total: Option<String>,
+    currency: String
 }
